a6fe72226781e188350396f3a330f56aef353ee4
rishabh-applore <rishabh.parashar@applore.in>
2023-01-02 12:02:56 +0530

    user jwt implemented --not-tested

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { register, login } = require("../services/users");
+const { register, login, jwtValidate, jwtAuth } = require("../services/users");
 const router = express.Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.post("/jwtAuth", jwtAuth);
+router.get("/jwtValidate/:token", jwtValidate);
 
 module.exports = router;
--- services/users.js
+++ services/users.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+let jwtSecretKey = process.env.JWT_SECRET_KEY;
 const { passwordEncryptor, passwordValidator } = require("../helpers/users");
 const User = require("../models/user");
 const {
@@ -76,5 +79,79 @@ exports.login = async (req, res) => {
     }
   } else {
     res.status(404).json({ message: "user not found" });
+  }
+};
+
+exports.jwtAuth = async (req, res) => {
+  // Validate User Here
+  // Then generate JWT Token
+
+  const credentials = req.body;
+  try {
+    const { error } = userLoginValidator.validate(credentials);
+    if (error) {
+      res
+        .status(400)
+        .json({ message: "User data validation failed", error: error.message });
+    }
+  } catch (err) {
+    res.status(400).json({
+      message: "error in validating the user inputs",
+      detail: err.message,
+    });
+  }
+  const foundUser = await User.findOne({
+    username: credentials.username,
+  }).catch((err) => {
+    res.status(404).json({ message: "User Info Not Found", details: err });
+  });
+  if (foundUser) {
+    const userValidated = await passwordValidator(
+      credentials.password,
+      foundUser.password
+    );
+    if (userValidated) {
+      let data = {
+        time: Date(),
+        userName: foundUser.userName,
+      };
+
+      const token = jwt.sign(data, jwtSecretKey);
+
+      res.status(200).json({
+        message: "User Found Successfully",
+        userData: { name: foundUser.name, role: foundUser.userName, token },
+      });
+    } else {
+      res.status(403).json({
+        message: "Incorrect Password",
+        dataSent: credentials,
+      });
+    }
+  } else {
+    res.status(404).json({ message: "user not found" });
+  }
+};
+
+exports.jwtValidate = (req, res) => {
+  // Tokens are generally passed in the header of the request
+  // Due to security reasons.
+  console.log(req.params);
+  const token = req.params.token;
+
+  console.log(token);
+
+  try {
+    // const token = req.header(tokenHeaderKey);
+    const verified = jwt.verify(token, jwtSecretKey);
+    if (verified) {
+      return res.send("Successfully Verified");
+    } else {
+      // Access Denied
+      return res.status(401).send(error);
+    }
+  } catch (error) {
+    // Access Denied
+    return res.status(401).send(error);
   }
 };
